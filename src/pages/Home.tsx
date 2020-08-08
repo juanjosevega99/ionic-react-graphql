c89e9693810695aff8616ccd0ea7211d210b706a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-07 23:10:20 -0500

    [FEATURE] one query to get a lot of data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,17 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonAvatar, IonLabel } from '@ionic/react';
 import React, { useEffect, useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-const GET_USERS = `
-query {
+const GET_USERS_ORDER = `
+query myQuery($orderId: ID!) {
   users: allUsers {
     id
     name
+  }
+  order(id: $orderId) {
+    id
+    createDateTime
   }
 }
 `
@@ -15,6 +19,7 @@ query {
 const Home: React.FC = () => {
 
   const [users, setUsers] = useState<any[]>([])
+  const [order, setOrder] = useState<any>({})
 
   useEffect(() => {
     fetch (
@@ -24,16 +29,17 @@ const Home: React.FC = () => {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          query: GET_USERS,
-          // variables: {
-
-          // }
+          query: GET_USERS_ORDER,
+          variables: {
+            orderId: 'xdxd'
+          }
         })
       }
     )
       .then(data => data.json())
       .then(response => {
         setUsers(response.data.users)
+        setOrder(response.data.order)
       })
   }, [])
 
@@ -45,6 +51,14 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+      <IonItem>
+        <IonAvatar slot="start">
+          <img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==" />
+        </IonAvatar>
+        <IonLabel>
+          {order.id}
+        </IonLabel>
+      </IonItem>
         <IonList>
           { users.map((user, index) => <IonItem key={index}>{user.name}</IonItem> )}
         </IonList>
